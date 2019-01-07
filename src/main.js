bffd202c2cb1c463801de89606fb94dc657e9b51
mlengse <anjangkn@gmail.com>
2019-01-07 15:18:44 +0700

    20190107

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import DefaultLayout from '~/layouts/Default.vue'
 import 'spectre.css/docs/dist/docs.css'
 import 'spectre.css/dist/spectre.css'
 import 'spectre.css/dist/spectre-exp.css'
 import 'spectre.css/dist/spectre-icons.css'
-export default function (Vue) {
-  Vue.component('Layout', DefaultLayout)
+export default function () {
 }
